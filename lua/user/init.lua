c6db3a607417851b2a209a934a79219c27a5cd46
Blinkov Ilya <blinkovilya@yandex-team.ru>
2023-05-28 20:13:18 +0300

    current astronvim config

---
+++ lua/user/init.lua
@@ -0,0 +1,36 @@
+vim.opt.guicursor = ""
+
+return {
+  lsp = {
+    formatting = {
+      format_on_save = true
+    }
+  },
+  mappings = {
+    i = {
+      ["hh"] = { "<Esc>", desc = "Go to normal mode" },
+      ["<C-k>"] = { "<Up>", desc = "Move cursor up" },
+      ["<C-j>"] = { "<Down>", desc = "Move cursor down" },
+      ["<C-h>"] = { "<Left>", desc = "Move cursor left" },
+      ["<C-l>"] = { "<Right>", desc = "Move cursor right" },
+    },
+
+    n = {
+      ["<C-s>"] = { ":w!<cr>", desc = "Save File" },
+      ["<C-j>"] = { "o<Esc>", desc = "New line under cursor" },
+      ["<C-k>"] = { "O<Esc>", desc = "New line over cursor" },
+      ["<Tab>"] = {
+        function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+        desc = "Next buffer"
+      },
+      ["<S-Tab>"] = {
+        function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+        desc = "Prev buffer"
+      },
+    },
+
+    t = {
+      ["hh"] = { "<C-\\><C-n>" }
+    }
+  }
+}
